20be54ab65c600a67d608f29973e88e4dadc4452
Parker Brandt <parker.brandt101@gmail.com>
2021-03-28 13:30:52 -0500

    Updated main() to be a skeleton version of what it will become

--- driver.c
+++ driver.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define BUF_SIZE 1024
+
 
 struct JSON
 {
@@ -12,7 +17,33 @@ typedef struct JSON JSONObject;
 int main(int argc, char **argv)
 {
 	// Variables
-	
+	char *filename;
+	char *buffer;
+	int fd;
+
+	JSONObject *json;
+
+	// Check command-line arguments
+	// Should be one that is the filename	
+	if(argc != 2)
+	{
+		printf("Illegal number of command-line arguments: %d\n", argc);
+	}
+	else
+	{
+		filename = argv[1];
+	}
+
+	// Open the file and read
+	fd = open(filename, O_RDONLY, 0);
+
+	buffer = (char*)malloc(BUF_SIZE * sizeof(char));
+
+	char c;
+	while(read(fd, &c, 1) > 0)
+	{
+
+	}	
 
 	exit(EXIT_SUCCESS);
 }
