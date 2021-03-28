26c36f48485ad855eed31051b039d53428faf873
Parker Brandt <parker.brandt101@gmail.com>
2021-03-28 13:25:42 -0500

    Initial commit

---
+++ driver.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct JSON
+{
+
+};
+typedef struct JSON JSONObject;
+
+
+// Main Function - Start of Program Logic
+int main(int argc, char **argv)
+{
+	// Variables
+	
+
+	exit(EXIT_SUCCESS);
+}
